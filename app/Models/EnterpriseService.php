ab84ecab3e7e1d5af047d0d6d6420933bfdb1beb
Flavio Jara R <flavio.jara.rebolledo@gmail.com>
2022-02-01 22:31:31 -0300

    created controllers and models

---
+++ app/Models/EnterpriseService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EnterpriseService extends Model
+{
+    use HasFactory;
+}
